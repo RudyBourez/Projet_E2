167cdfff49d1867f50d1ddca8b923fc1c4312e58
Rudy <bourez.rudy@gmail.com>
2022-11-07 17:01:56 +0100

    amélioration form

--- Flask/app/tests/test_app.py
+++ Flask/app/tests/test_app.py
@@ -6,23 +6,44 @@ def client():
     with app.test_client() as client:
         yield client   
 
+
+# -----------------------------------V1---------------------------------------------
 def test_index(client):
     response = client.get('/')
     assert b'<h1> Welcome </h1>' in response.data
     assert response.status_code == 200
+    
+def test_predict_logged_in(client):
+    email = 'user@example.com'
+    password = '123456'
+    client.post('/login', data={'email' : email, 'password' : password})
+    response = client.get('/predict')
+    assert response.status_code == 200
+    
+def test_prediction(client):
+    email = 'user@example.com'
+    password = '123456'
+    client.post('/login', data={'email' : email, 'password' : password})
+    response = client.post('/predict', data={'Year_Built':2000, 'Total_Bsmt_SF':50, '1st_Flr_SF':50,
+                                             'Gr_Liv_Area':50,'Garage_Area':20, 'Overall_Qual':5,
+                                             'Full_Bath':2, 'Exter_Qual':"TA",'Kitchen_Qual': "TA",
+                                             'Neighborhood': "NWAmes"})
+    assert b'<p> La maison vaut: 46534</p>' in response.data
  
+# -----------------------------------V2---------------------------------------------
+
 def test_signup(client):
     response = client.get('/signup')
-    assert b'<form method="POST" action="/signup">' in response.data
+    assert b'<form method="POST" action="/signup" class="form">' in response.data
     assert response.status_code == 200
 
-def test_profile_without_login(client):
+def test_predict_without_login(client):
 	response = client.get('/predict')
 	assert response.status_code == 302
 
 def test_login(client):
     response = client.get('/login')
-    assert b'<form method="POST" action="/login">' in response.data
+    assert b'<form method="POST" action="/login" class="form">' in response.data
     assert response.status_code == 200
  
 def test_with_login(client):
@@ -55,20 +76,3 @@ def test_logout_logged_out(client):
 def test_predict_not_logged_in(client):
     response = client.get('/predict')
     assert response.status_code == 302
-    
-def test_predict_logged_in(client):
-    email = 'user@example.com'
-    password = '123456'
-    client.post('/login', data={'email' : email, 'password' : password})
-    response = client.get('/predict')
-    assert response.status_code == 200
-
-def test_prediction(client):
-    email = 'user@example.com'
-    password = '123456'
-    client.post('/login', data={'email' : email, 'password' : password})
-    response = client.post('/predict', data={'Year_Built':2000, 'Total_Bsmt_SF':50, '1st_Flr_SF':50,
-                                             'Gr_Liv_Area':50,'Garage_Area':20, 'Overall_Qual':5,
-                                             'Full_Bath':2, 'Exter_Qual':"TA",'Kitchen_Qual': "TA",
-                                             'Neighborhood': "NWAmes"})
-    assert b'<p> La maison vaut: 46534</p>' in response.data
